8e39ba402f45fdf4af004210a39bfb940d6045c0
Raul Andrial <rauljandrial@gmail.com>
2024-08-27 18:24:08 -0400

    cors issue solved

--- server.js
+++ server.js
@@ -1,4 +1,24 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');;
 const app = express();
-console.log(process.env.TEST)
+app.use(express.json());
+
+const corsOptions = {
+    orgin: ['http://localhost:3000'],
+    methods: ['POST'],
+    allowedHeaders: ['Content-Type']
+}
+
+app.use(cors(corsOptions));
+
+app.post('/rating', (req, res) => {
+    const { body } = req;
+    console.log(body.userMsg);
+    console.log(body.botMsg);
+    console.log(body.ratingValue);
+    res.send({status: "It worked!"});
+});
+
+app.listen(process.env.PORT);
+
