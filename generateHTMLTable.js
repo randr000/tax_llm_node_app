aaf23b480fb3748b47eb2832d750fbd4d8dedb8e
Raul Andrial <rauljandrial@gmail.com>
2024-08-28 11:59:39 -0400

    changed generateHTMLTable to return a promise

--- generateHTMLTable.js
+++ generateHTMLTable.js
@@ -2,27 +2,27 @@ const fs = require('fs');
 const { selectAll } = require('./mysqlQueries');
 
 function generateHTMLTable(rows) {
-    if (!rows) return;
+    return new Promise((resolve, reject) => {
+        if (!rows) resolve(false);
 
-    const tHeads = ['rating_id', 'rating_created_at', 'user_query', 'bot_response', 'rating'];
+        const tHeads = ['rating_id', 'rating_created_at', 'user_query', 'bot_response', 'rating'];
 
-    let HTML = '';
+        let HTML = '';
 
-    const begTableHTML = '<table><caption>Chatbot Response Ratings</caption>';
-    const tHeadsHTML = `<thead><tr>${tHeads.map(v => `<th>${v}</th>`).join('')}</tr></thead>`;
-    const tRowsHTML = `<tbody>${rows.map(row => `<tr>${tHeads.map(k => `<td>${row[k] ? row[k] : ''}</td>`).join('')}</tr>`).join('')}</tbody>`;
-    const tFooterHTML = `<tfoot><tr><td colspan="${tHeads.length}">2023 IRS Publication 17 Chatbot Response Ratings</td></tr></tfoot>`;
-    const endTableHTML = '</table>';
+        const begTableHTML = '<table><caption>Chatbot Response Ratings</caption>';
+        const tHeadsHTML = `<thead><tr>${tHeads.map(v => `<th>${v}</th>`).join('')}</tr></thead>`;
+        const tRowsHTML = `<tbody>${rows.map(row => `<tr>${tHeads.map(k => `<td>${row[k] ? row[k] : ''}</td>`).join('')}</tr>`).join('')}</tbody>`;
+        const tFooterHTML = `<tfoot><tr><td colspan="${tHeads.length}">2023 IRS Publication 17 Chatbot Response Ratings</td></tr></tfoot>`;
+        const endTableHTML = '</table>';
 
-    const endHTML = '</body></html>';
+        const endHTML = '</body></html>';
 
-    fs.readFile('mailerEmailHTML/begHTML.html', 'utf8', (error, begHTML) => {
-        if (error) console.log(error);
-        else {
-            HTML = `${begHTML}${begTableHTML}${tHeadsHTML}${tRowsHTML}${tFooterHTML}${endTableHTML}${endHTML}`
-            console.log(HTML);
-        }
+        fs.readFile('mailerEmailHTML/begHTML.html', 'utf8', (error, begHTML) => {
+            if (error) reject(error);
+            else resolve(`${begHTML}${begTableHTML}${tHeadsHTML}${tRowsHTML}${tFooterHTML}${endTableHTML}${endHTML}`);
+        });
     });
+
 }
 
-selectAll().then(rows => generateHTMLTable(rows));
+selectAll().then(rows => generateHTMLTable(rows)).then(r => console.log(r));
