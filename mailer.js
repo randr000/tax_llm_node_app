1d7967dec998b117e23a0eacded9856a6f09effe
Raul Andrial <rauljandrial@gmail.com>
2024-08-27 21:09:00 -0400

    can convert database responses from select all query to csv

--- mailer.js
+++ mailer.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
+const nodemailer = require('nodemailer');
+const converter = require('json-2-csv');
 const { selectAll, deleteAll } = require('./mysqlQueries');
 
 
-selectAll().then(res => console.log(res));
+// selectAll().then(res => console.log(res));
+
+selectAll().then(res => converter.json2csv(res, {checkSchemaDifferences: true, emptyFieldValue: null})).then(res => console.log(res));
