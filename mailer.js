f9c3de38f3019bd7f90ad30924aef3a7c76c1149
Raul Andrial <rauljandrial@gmail.com>
2024-08-28 00:44:13 -0400

    fixed bug of app crashing after second insert sent to sql database

--- mailer.js
+++ mailer.js
@@ -6,4 +6,11 @@ const { selectAll, deleteAll } = require('./mysqlQueries');
 
 // selectAll().then(res => console.log(res));
 
-selectAll().then(res => converter.json2csv(res, {checkSchemaDifferences: true, emptyFieldValue: null})).then(res => console.log(res));
+// selectAll().then(res => console.log(converter.json2csv(res, {checkSchemaDifferences: true, emptyFieldValue: null})));
+// deleteAll();
+
+(async () => {
+    return converter.json2csv(await selectAll(), {checkSchemaDifferences: true, emptyFieldValue: null, delimiter: {field: "|"}});
+})().then(res => console.log(res));
+
+// deleteAll();
--- mysqlQueries.js
+++ mysqlQueries.js
@@ -4,19 +4,21 @@ const mysql = require('mysql2');
 const {MYSQL_DATABASE, MYSQL_USER, MYSQL_PASSWORD, MY_SQL_HOST, MY_SQL_PORT} = process.env;
 connTimeout = {timeout: 100_000};
 
-const connection = mysql.createConnection({
-    host: MY_SQL_HOST,
-    port: MY_SQL_PORT,
-    user: MYSQL_USER,
-    password: MYSQL_PASSWORD,
-    database: MYSQL_DATABASE,
-});
+function createConn() {
+    return mysql.createConnection({
+        host: MY_SQL_HOST,
+        port: MY_SQL_PORT,
+        user: MYSQL_USER,
+        password: MYSQL_PASSWORD,
+        database: MYSQL_DATABASE,
+    });
+}
 
-function esc(value) {
+function esc(value, connection) {
     return connection.escape(value);
 }
 
-function handleConnections(func) {
+function handleConnections(func, connection) {
     connection.connect();
     func();
     connection.end();
@@ -25,6 +27,7 @@ function handleConnections(func) {
 function selectAll() {
 
     return new Promise((resolve, reject) => {
+        const connection = createConn();
         const query = () => {
             const sql = "SELECT * FROM query_ratings;";
             connection.query({...connTimeout, sql: sql}, (error, results, fields) => {
@@ -37,25 +40,27 @@ function selectAll() {
                 };
             });
         };
-        handleConnections(query);
+        handleConnections(query, connection);
     });
 }
 
 function insertInto({userMsg, botMsg, ratingValue}) {
+    const connection = createConn();
     const query = () => {
         const sql = `
         INSERT INTO query_ratings (user_query, bot_response, rating)
-        VALUES (${esc(userMsg)}, ${esc(botMsg)}, ${esc(ratingValue)});
+        VALUES (${esc(userMsg, connection)}, ${esc(botMsg, connection)}, ${esc(ratingValue, connection)});
         `;
         connection.query({...connTimeout, sql: sql}, (error, results, fields) => {
             if (error) console.log(error);
             else console.log('Insert was successful');
         });
     };
-    handleConnections(query);
+    handleConnections(query, connection);
 }
 
 function deleteAll() {
+    const connection = createConn();
     const query = () => {
         const sql = "DELETE FROM query_ratings;";
         connection.query({...connTimeout, sql: sql}, (error, results, fields) => {
@@ -63,7 +68,7 @@ function deleteAll() {
             else console.log('Delete was successful');
         });
     };
-    handleConnections(query);
+    handleConnections(query, connection);
 }
 
 module.exports = {
